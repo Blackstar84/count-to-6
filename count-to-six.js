dfaf4e44bd550a9615a9f3591ac4b7e847e2992d
Blackstar84 <carlosalmada84@gmail.com>
2023-01-11 07:56:15 -0300

    se agrega la solución a two sums

--- count-to-six.js
+++ count-to-six.js
@@ -554,5 +554,54 @@ The outline of your program should look something like this:
     console.log(html(`<b>${process.argv[2]} says</b>:`,`${process.argv[3]}`));
 
 
+/**
+ * 1. Two Sum
+Easy
+42K
+1.4K
+Companies
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ */
     
+
+
+
+    var twoSum = function(nums, target) {    
+        for (let i=0; i < nums.length; i++) {
+            const resta = target - nums[i];        
+            const encontrar = nums.indexOf(resta);                
+            if (encontrar !== -1 && encontrar !== i) {
+                return [i, encontrar];
+            }
+        }
+    }; 
+    
+    console.log(twoSum([2,5,5,11], 10));
